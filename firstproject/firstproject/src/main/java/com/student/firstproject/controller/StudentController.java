8f080af97c4c0277e05a659450bb98442cee3f25
Alfazal007 <alfazal7777@gmail.com>
2023-08-27 10:51:55 +0530

    entity created

---
+++ firstproject/firstproject/src/main/java/com/student/firstproject/controller/StudentController.java
@@ -0,0 +1,4 @@
+package com.student.firstproject.controller;
+
+public class StudentController {
+}
--- firstproject/firstproject/src/main/java/com/student/firstproject/entity/Student.java
+++ firstproject/firstproject/src/main/java/com/student/firstproject/entity/Student.java
@@ -1,4 +1,57 @@
 package com.student.firstproject.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String phone;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
